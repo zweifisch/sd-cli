53af6c8ee306462cc70b978f07020414f1bfc912
zweifisch <zf.pascal@gmail.com>
2024-03-17 16:46:22 +0800

    feat: IP Composition Adapter

--- src/sd_tools/plugins/ip_composition.py
+++ src/sd_tools/plugins/ip_composition.py
@@ -1,41 +1,40 @@
-import torch
+import os
 from argparse import ArgumentParser
 from diffusers import DDIMScheduler
 from .base import PluginBase
-from .utils import load_images
+from .utils import hf_download
+from diffusers.utils import load_image
 
 
 class PluginIPCompositionAdapter(PluginBase):
 
     def setup_args(self, parser: ArgumentParser):
         group = parser.add_argument_group('IP Composition Adapter')
-        group.add_argument("--ip-composition", type=str, nargs='+', help="Reference image")
+        group.add_argument("--ip-composition", type=str, help="Reference image")
         group.add_argument("--ip-composition-scale", type=float, default=0.6, help="IPAdapter scale")
 
     def setup(self):
         if not self.ctx.args.ip_composition:
             return
 
-        from transformers import CLIPVisionModelWithProjection
-        self.ctx.pipeline_opts_extra['image_encoder'] = CLIPVisionModelWithProjection.from_pretrained(
-            "h94/IP-Adapter",
-            subfolder="models/image_encoder",
-            torch_dtype=torch.float16,
-            local_files_only=self.ctx.offline,
-            resume_download=not self.ctx.offline,
-        )
-
     def setup_pipe(self):
         if not self.ctx.args.ip_composition:
             return
 
-        pipe = self.ctx.pipe
-        pipe.scheduler = DDIMScheduler.from_config(pipe.scheduler.config)
-        pipe.set_ip_adapter_scale(self.ctx.args.ip_composition_scale)
-        self.ctx.pipe_opts_extra['ip_adapter_image'] = load_images(self.ctx.args.ip_composition)
+        from .ip_adapter.ip_adapter import IPAdapterPlus
 
-        pipe.load_ip_adapter(
-            "ostris/ip-composition-adapter",
-            subfolder='',
-            weight_name="ip_plus_composition_sd15.safetensors",
+        self.ctx.pipe.scheduler = DDIMScheduler.from_config(self.ctx.pipe.scheduler.config)
+
+        hf_download("h94/IP-Adapter/models/image_encoder/config.json", offline=self.ctx.offline)
+        image_encoder_path = os.path.dirname(hf_download("h94/IP-Adapter/models/image_encoder/model.safetensors", offline=self.ctx.offline))
+
+        self.ctx.pipe = IPAdapterPlus(
+            self.ctx.pipe,
+            image_encoder_path,
+            hf_download("ostris/ip-composition-adapter/ip_plus_composition_sd15.safetensors"),
+            self.ctx.device,
+            num_tokens=16
         )
+
+        self.ctx.pipe_opts_extra['pil_image'] = load_image(self.ctx.args.ip_composition)
+        self.ctx.pipe_opts_extra['scale'] = self.ctx.args.ip_composition_scale
